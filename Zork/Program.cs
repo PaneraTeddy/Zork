f0cf0c5722aa803839382561a5003856a9a52018
PaneraTeddy <47015328+PaneraTeddy@users.noreply.github.com>
2020-09-19 19:29:12 -0400

    2.3 done
    fix the code to match with 2.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,157 +1,121 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Data.Common;
 
 namespace Zork
 {
     class Program
     {
-        private static readonly string[,] Rooms = 
+        private static readonly string[,] Rooms =
         {
             { "Rocky Trail", "South of House", "Canyon View" },
             { "Forest", "West of House", "Behind House" },
             { "Dense Woods", "North of House", "Clearing" },
 
         };
+        private static string CurrentRoom
+        {
+            get
+            {
+                return Rooms[Location.Row, Location.Column];
+            }
 
-        private static (int row, int col) Location;
+        }
 
-        private static bool moveIsVaild = false;
-
+        // private static (int row, int col) Location;
+        // private static bool moveIsVaild = false;
         static void Main(string[] args)
         {
-            Location.col = 1;
-            Location.row = 1;
+            // Location.col = 1;
+            // Location.row = 1;
 
             Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
             {
-                Console.WriteLine(Rooms[Location.row, Location.col]);
+                Console.WriteLine(CurrentRoom);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
                 // check if the move is vaild
-                moveIsVaild = CanMove(command);
+                // moveIsVaild = CanMove(command);
                 // Console.Write("You at : " + currentRoom + "moveIsVaild :" + moveIsVaild + "\n");
-                string outputString;
+                // string outputString;
                 switch (command)
-                {
+                {       
+                    case Commands.QUIT:
+                        Console.WriteLine("Thank you for playing!");
+                        break;
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door.\n" +
-                        "A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\n" +
+                        "A rubber mat saying 'Welcome to Zork!' lies by the door.");
                         break;
 
                     case Commands.NORTH:
-                        if (moveIsVaild == true)
-                        {
-                            Location.row++;
-                            outputString = "You moved " + command + ".";
-                        }
-                        else
-                        {
-                            outputString = "The way is shut!";
-                        }
-                        break;
                     case Commands.SOUTH:
-                        if (moveIsVaild == true)
-                        {
-                            Location.row--;
-                            outputString = "You moved " + command + ".";
-                        }
-                        else
-                        {
-                            outputString = "The way is shut!";
-                        }
-                        break;
                     case Commands.EAST:
-                        if (moveIsVaild == true)
-                        {
-                            Location.col++;
-                            outputString = "You moved " + command + ".";
-                        }
-                        else
-                        {
-                            outputString = "The way is shut!";
-                        }
-                        break;
-                            
                     case Commands.WEST:
-                        if (moveIsVaild == true)
+                        if (Move(command) == false)
                         {
-                            Location.col--;
-                            outputString = "You moved " + command + ".";
-                        }
-                        else
-                        {
-                            outputString = "The way is shut!";
+                            Console.WriteLine("The way is shut");
                         }
                         break;
-
-                    case Commands.QUIT:
-                        outputString = "Thank you for playing!";
-                        break;
-
                     default:
-                        outputString = "Unkown command.";
+                        Console.WriteLine("Unkown command.");
                         break;
                 }
-                Console.WriteLine(outputString);
             }  
         }
            
-        private static bool CanMove (Commands command)
-        {   
+        private static bool Move (Commands command)
+        {
+            Assert.IsTrue(IsDirection(command), "Invalid direction.");
+            bool isValidMove = true;
+
             switch(command)
             {
-                case Commands.NORTH:
-                    if (Location.row + 1 >= Rooms.GetLength(0))
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
+                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
+                    Location.Row++;
+                    break;
 
-                case Commands.SOUTH:
-                    if (Location.row - 1 <= -1)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
+                case Commands.SOUTH when Location.Row > 0:
+                    Location.Row--;
+                    break;
 
-                case Commands.EAST:
-                    if (Location.col + 1 >= Rooms.GetLength(1))
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
+                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
+                    Location.Column++;
+                    break;
 
-                case Commands.WEST:
-                    if (Location.col - 1 <= -1)
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        return true;
-                    }
+                case Commands.WEST when Location.Column > 0:
+                    Location.Column--;
+                    break;
+
                 default:
-                   return false;
+                   isValidMove = false;
+                    break;
             }
+
+            return isValidMove;
+        }
+        private static bool IsDirection(Commands command)
+        {
+            return Directions.Contains(command);
         }
 
         private static Commands ToCommand(string commandString) => 
         (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
- 
 
+        private static readonly List<Commands> Directions = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
+
+        private static (int Row, int Column) Location = (1, 1);
     }
    
 }
