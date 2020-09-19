c99580cc5fe88b29b97dd7a80c3486639630a72c
PaneraTeddy <47015328+PaneraTeddy@users.noreply.github.com>
2020-09-19 14:46:02 -0400

    2.1 compeleted
    2.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace Zork
 {
     class Program
     {
+        private static string[] Rooms = { "Forest", " West of the House", "Behind House", "Clearing", "Canyon View" };
+        private static int currentRoom = 1;
+        private static bool moveIsVaild = false;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
@@ -11,22 +16,42 @@ namespace Zork
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
             {
-                Console.Write("> ");
+                Console.WriteLine(Rooms[currentRoom]);
                 command = ToCommand(Console.ReadLine().Trim());
-
+                moveIsVaild = CanMove(command);
+                
                 string outputString;
                 switch (command)
                 {
                     case Commands.LOOK:
                         outputString = "This is an open field west of a white house, with a boarded front door.\n" +
-                            "A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        "A rubber mat saying 'Welcome to Zork!' lies by the door.";
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
+                        if (moveIsVaild == true)
+                        { 
+                            currentRoom ++;
+                            outputString = "You moved " + command + ".";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        break;
+                            
                     case Commands.WEST:
-                        outputString = "You moved " + command + ".";
+                        if (moveIsVaild == true)
+                        { 
+                            currentRoom ++;
+                            outputString = "You moved " + command + ".";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
                         break;
 
                     case Commands.QUIT:
@@ -37,12 +62,48 @@ namespace Zork
                         outputString = "Unkown command.";
                         break;
                 }
-
                 Console.WriteLine(outputString);
+            }  
+        }
+           
+        private static bool CanMove (Commands command)
+        {   
+            int moveBond = Rooms.Length;
+
+            switch(command)
+            {
+                case Commands.NORTH:
+                case Commands.SOUTH:
+                    return false;
+                    
+                case Commands.EAST:
+                    if (currentRoom + 1 >= moveBond)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case Commands.WEST:
+                    if (currentRoom - 1 < moveBond)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                default:
+                   return false;
             }
         }
 
         private static Commands ToCommand(string commandString) => 
-            (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+        (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+ 
+
     }
+   
 }
+
