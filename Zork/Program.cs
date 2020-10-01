380bf0b519d7df3f5b035f073655cb5e3fcac9f9
PaneraTeddy <47015328+PaneraTeddy@users.noreply.github.com>
2020-10-01 11:18:12 -0400

    3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,14 +7,7 @@ namespace Zork
 {
     class Program
     {
-        private static readonly string[,] Rooms =
-        {
-            { "Rocky Trail", "South of House", "Canyon View" },
-            { "Forest", "West of House", "Behind House" },
-            { "Dense Woods", "North of House", "Clearing" },
-
-        };
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -31,6 +24,7 @@ namespace Zork
             // Location.row = 1;
 
             Console.WriteLine("Welcome to Zork!");
+            InitializeRoomDescriptions();
 
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
@@ -39,18 +33,10 @@ namespace Zork
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                // check if the move is vaild
-                // moveIsVaild = CanMove(command);
-                // Console.Write("You at : " + currentRoom + "moveIsVaild :" + moveIsVaild + "\n");
-                // string outputString;
                 switch (command)
                 {       
-                    case Commands.QUIT:
-                        Console.WriteLine("Thank you for playing!");
-                        break;
                     case Commands.LOOK:
-                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\n" +
-                        "A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.NORTH:
@@ -61,6 +47,9 @@ namespace Zork
                         {
                             Console.WriteLine("The way is shut");
                         }
+                        break;
+                    case Commands.QUIT:
+                        Console.WriteLine("Thank you for playing!");
                         break;
                     default:
                         Console.WriteLine("Unkown command.");
@@ -106,6 +95,28 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) => 
         (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
+            Rooms[0, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windowss are barred.";
+            Rooms[0, 2].Description = "You are at the top of the great canyon on its south wall.";
+
+            Rooms[1, 0].Description = "This is a forest with trees in all direction around you";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door";
+            Rooms[1, 2].Description = "You are behind the white house. In one cornner of the house there is a small window which is slightly ajar";
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred."; // south
+            Rooms[2, 2].Description = " You are in a clearing, with a forest surrounding you on the west and south.";
+        }
+
+        private static readonly Room[,] Rooms = {
+            { new Room ("Rocky Trail"), new Room ("North of House"),new Room ("Canyon View") },
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room("Dense Woods"), new Room("South of House"), new Room("Clearing" )},
+
+        };
 
         private static readonly List<Commands> Directions = new List<Commands>
         {
